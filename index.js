a66397c8c79df61f1a32adaedb8a86946c5605c3
VairamuthuDG <vairamuthuvgd@gmail.com>
2022-01-10 01:16:19 +0530

    fourth

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ window.addEventListener('DOMContentLoaded',function(){
     })
 
     
-    
     const btnEl  = document.querySelector('.btn');
     const showEdu = document.querySelector('.show-edu');
     const closeSch = document.querySelector('.close-school');
@@ -216,6 +215,24 @@ window.addEventListener('DOMContentLoaded',function(){
         
         //Questions
 
+        mybutton = document.getElementById("scroll-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener('click',function(){
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+})
         });
 
 
